635cbb4014cf55c264f6832e59f2fd0db7685ab9
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:10:06 +0700

    PboUlang-10116413-Latihan35-ProgramTunjangan

---
+++ src/pboulang/pkg10116413/latihan35/programtunjangan/PboUlang10116413Latihan35ProgramTunjangan.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan35.programtunjangan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : program ini berisi program yang menampilkan program
+ * tunjangan pegawai
+ */
+public class PboUlang10116413Latihan35ProgramTunjangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+        Karyawan kar = new Karyawan();
+
+        System.out.println("===========Program Tunjangan===========");
+        System.out.print("Berapa gaji pokok Anda perbulan ? : Rp. ");
+        kar.gajiPokok = scn.nextDouble();
+        System.out.print("Status Anda? (Menikah/Belum) :");
+        kar.status = scn.next();
+
+        kar.HasilHitung(kar.status, kar.gajiPokok);
+
+    }
+
+}
